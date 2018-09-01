828d03019a52efba655a5c81b7b6d92e3b677f20
Galina Egrashina <egrashina.gi@gmail.com>
2018-09-01 14:26:06 +0300

    инициализировать репозиторий
    - Инициализированно приложение create-react-app.
    - Создан функциональный глупый компонент для примера.

---
+++ src/components/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {MyComp} from './my-comp.jsx'
+
+export class App extends React.Component {
+   render () {
+       return(
+           <div>
+               <MyComp />
+           </div>
+       )
+   }
+}
